1117f98974fa5b7dbecdb135ce76a302c5044736
helloxubaer <xubaer.xeo@gmail.com>
2021-10-23 22:35:39 +0200

    custom access denied page

---
+++ src/main/java/com/mycompany/storemanager/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.mycompany.storemanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/access-denied")
+	public String accessDeniedToTheResource() {
+		return "access-denied";
+	}
+}
